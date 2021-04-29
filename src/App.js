f45aa5fd148923d883f613b282f6d13aa7072d30
ares4004 <stinger10559@gmail.com>
2021-04-29 20:40:16 +0530

    create a form and submit it on server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onButtonClick = () => {
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ email: email, password: password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Form</div>
+      <input
+        placeholder="email"
+        onChange={(event) => setEmail(event.target.value)}
+      ></input>
+      <input
+        placeholder="password"
+        onChange={(event) => setPassword(event.target.value)}
+      ></input>
+      <button onClick={onButtonClick}>Send</button>
     </div>
   );
 }
